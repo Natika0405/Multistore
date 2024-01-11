7270e6d842b3a9d331bd36fff60f8f8f0cd85ec8
nurazizahs <natikasavitha2@gmail.com>
2024-01-11 20:53:43 +0700

    integration category dan product

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:multistore_app/pages/auth/auth_page.dart';
-import 'package:multistore_app/pages/dashboard/dashboard_page.dart';
-import 'package:multistore_app/pages/splash/splash_page.dart';
-import 'package:multistore_app/utils/light_themes.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_fic7_app/bloc/categories/categories_bloc.dart';
+import 'package:flutter_fic7_app/bloc/logout/logout_bloc.dart';
+import 'package:flutter_fic7_app/bloc/products/products_bloc.dart';
+import 'package:flutter_fic7_app/data/datasources/auth_local_datasource.dart';
+import 'package:flutter_fic7_app/pages/auth/auth_page.dart';
+import 'package:flutter_fic7_app/pages/dashboard/dashboard_page.dart';
+import 'package:flutter_fic7_app/pages/splash/splash_page.dart';
+import 'package:flutter_fic7_app/utils/light_themes.dart';
+
+import 'bloc/login/login_bloc.dart';
+import 'bloc/register/register_bloc.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,10 +21,43 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: light,
-      home: const DashboardPage(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => RegisterBloc(),
+        ),
+        BlocProvider(
+          create: (context) => LoginBloc(),
+        ),
+        BlocProvider(
+          create: (context) => LogoutBloc(),
+        ),
+        BlocProvider(
+          create: (context) => ProductsBloc(),
+        ),
+        BlocProvider(
+          create: (context) => CategoriesBloc(),
+        ),
+      ],
+      child: MaterialApp(
+          title: 'Flutter Demo',
+          theme: light,
+          home: FutureBuilder<bool>(
+            future: AuthLocalDatasource().isLogin(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Scaffold(
+                  body: Center(
+                    child: CircularProgressIndicator(),
+                  ),
+                );
+              } else if (snapshot.hasData && snapshot.data!) {
+                return const DashboardPage();
+              } else {
+                return const AuthPage();
+              }
+            },
+          )),
     );
   }
 }
